2ae4b8cc95fec028f8249bcd7819835e900d9c09
nguyen thanh tan <thanhtanpm2000@gmail.com>
2020-11-19 22:47:28 +0700

    edit hinh

--- QuanLyCanTeen/Models/Model.Designer.cs
+++ QuanLyCanTeen/Models/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\For Learning\VanLang\C#\QuanLyCanTeen\QuanLyCanTeen\QuanLyCanTeen\Models\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\thanh tan\Downloads\CanTeenVLU_ASPMVC\QuanLyCanTeen\Models\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
